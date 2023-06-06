d310dcb344909f8d95eaf573a0e990d855223633
B Marques <brunosmarques1995@hotmail.com>
2023-06-05 23:09:44 -0300

    now there are a sender and a receiver

--- tcp_client.py
+++ tcp_client.py
@@ -1,34 +1,59 @@
 import socket
+import threading
 
 # Define the server host and port
 HOST = 'localhost'
 PORT = 12345
 
-# Define the termination code
-TERMINATION_CODE = 'exit'
+def SenderClient():
 
-# Create a TCP socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Create a TCP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server
-client_socket.connect((HOST, PORT))
-print(f"Connected to {HOST}:{PORT}")
+    # Connect to the server
+    client_socket.connect((HOST, PORT))
+    print(f"Connected to {HOST}:{PORT}")
 
-while True:
-    # Get user input
-    message = input("Enter a message ('exit' to terminate): ")
+    while True:
+        # Get user input
+        level_input = float(input("Enter the tank level: "))
 
-    # Send data to the server
-    client_socket.send(message.encode('utf-8'))
+        # Send data to the server
+        client_socket.send(str(level_input).encode('utf-8'))
 
-    # Check if termination code sent
-    if message.strip().lower() == TERMINATION_CODE:
-        print("Termination code sent. Closing the connection.")
-        break
+        # Check if termination code sent
+        if level_input == 0.0:
+            print("Termination code sent. Closing the connection.")
+            break
 
-    # Receive and print the server's response
-    response = client_socket.recv(1024).decode('utf-8')
-    print(f"Received response: {response}")
+    # Close the connection
+    client_socket.close()
 
-# Close the connection
-client_socket.close()
+def ReceiverClient():
+
+    # Create a TCP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to the server
+    client_socket.connect((HOST, PORT+1))
+    print(f"Connected to {HOST}:{PORT}")
+
+    while True:
+
+        # Receive and print the server's response
+        response = client_socket.recv(1024).decode('utf-8')
+        print(f"Received response: {response}")
+
+    # Close the connection
+    client_socket.close()
+
+# Create and start the threads
+thread1 = threading.Thread(target=SenderClient)
+thread2 = threading.Thread(target=ReceiverClient)
+
+thread1.start()
+thread2.start()
+
+# Wait for the threads to finish
+thread1.join()
+thread2.join()
--- tcp_server.py
+++ tcp_server.py
@@ -1,40 +1,93 @@
 import socket
+import threading
 
 # Define the server host and port
 HOST = 'localhost'
 PORT = 12345
 
-# Define the termination code
-TERMINATION_CODE = 'exit'
+level_input = 0.0
+mutex = threading.Lock()
 
-# Create a TCP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def ReceiverServer():
 
-# Bind the socket to the host and port
-server_socket.bind((HOST, PORT))
+    global level_input
 
-# Listen for incoming connections
-server_socket.listen(1)
-print(f"Server listening on {HOST}:{PORT}")
+    # Create a TCP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Accept a client connection
-client_socket, client_address = server_socket.accept()
-print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+    # Bind the socket to the host and port
+    server_socket.bind((HOST, PORT))
 
-while True:
-    # Receive data from the client
-    data = client_socket.recv(1024).decode('utf-8')
-    print(f"Received data: {data}")
+    # Listen for incoming connections
+    server_socket.listen(1)
+    print(f"Server listening on {HOST}:{PORT}")
 
-    # Check if termination code received
-    if data.strip().lower() == TERMINATION_CODE:
-        print("Termination code received. Closing the connection.")
-        break
+    # Accept a client connection
+    client_socket, client_address = server_socket.accept()
+    print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
 
-    # Send a response to the client
-    response = "Server received: " + data
-    client_socket.send(response.encode('utf-8'))
+    while True:
+        # Receive data from the client
+        data = client_socket.recv(1024).decode('utf-8')
+        mutex.acquire()
+        level_input = float(data)
+        mutex.release()
+        print(f"Received data: {data}")
 
-# Close the connection
-client_socket.close()
-server_socket.close()
+        # Check if termination code received
+        if data == 0.0:
+            print("Termination code received. Closing the connection.")
+            break
+
+    # Close the connection
+    client_socket.close()
+    server_socket.close()
+
+def SenderServer():
+    global level_input
+
+    # Create a TCP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the host and port
+    server_socket.bind((HOST, PORT+1))
+
+    # Listen for incoming connections
+    server_socket.listen(1)
+    print(f"Server listening on {HOST}:{PORT}")
+
+    # Accept a client connection
+    client_socket, client_address = server_socket.accept()
+    print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+
+    while True:
+
+        # Check if termination code received
+        mutex.acquire()
+        data = level_input
+        mutex.release()
+
+        if data == 0.0:
+            print("Termination code received. Closing the connection.")
+            break
+
+        response = "Server received: "
+        mutex.acquire()
+        response = "Server received: " + str(level_input)
+        mutex.release()
+        client_socket.send(response.encode('utf-8'))
+
+    # Close the connection
+    client_socket.close()
+    server_socket.close()
+
+# Create and start the threads
+thread1 = threading.Thread(target=ReceiverServer)
+thread2 = threading.Thread(target=SenderServer)
+
+thread1.start()
+thread2.start()
+
+# Wait for the threads to finish
+thread1.join()
+thread2.join()
